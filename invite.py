e3e4e0cbd9ce2a3ece41a2b8d07194f98b322cf9
PEdruuDEV <62157321+PEdruuDEV@users.noreply.github.com>
2020-03-15 11:31:14 -0300

    Update invite.py

--- invite.py
+++ invite.py
@@ -12,7 +12,6 @@ async def on_ready():
 	global role_list
 	print('Logged in as: '+client.user.name)
 	print('Bot ID: '+client.user.id)
-	await client.change_presence(game=discord.Game(name='Teste'))
 	print('------\n')
 	for server in client.servers:
 		role_list=dict((role.name,role) for role in server.roles)
